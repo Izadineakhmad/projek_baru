e19c78f4dcff0bdb1aee5b9d6db9a4eda9761bea
Galsans <glhsmk21357@gmail.com>
2023-02-26 19:42:05 +0700

    add file program_kejurusan

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     setTimeout(function(){
       $('body').addClass('loaded');
       $('h1').css('color','#222222')
-    }, 1200);
+    }, 900);
   });
 
 // NAVBAR DROPDOWN
